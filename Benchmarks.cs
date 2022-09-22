bd54b58348b5010a210b08758312bc2874fce472
paul van bladel <paul.van.bladel@immothekerfinotheker.be>
2022-09-22 16:12:14 +0200

    Add project files.

---
+++ Benchmarks.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BenchmarkDotNet.Attributes;
+using System.Collections.Generic;
+
+[MemoryDiagnoser(false)]
+public class Benchmarks
+{
+    private static List<int> _list;
+
+
+
+    [Params(100, 100_000, 1_000_000)]
+    public int Size { get; set; }
+
+    [GlobalSetup]
+    public void GlobalSetup()
+    {
+        _list = Enumerable.Range(1, Size).ToList();
+        
+    }
+
+    [Benchmark]
+    public void TestHashSetApproach()
+    {
+        var result = _list.ContainsDuplicatesHashSetApproach();
+    }
+    [Benchmark]
+    public void TestContainsDuplicatesDistinctCountApproach()
+    {
+        var result = _list.ContainsDuplicatesDistinctCountApproach();
+    }
+}
---
+++ DuplicateExtensions.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+public static class DuplicateExtensions
+{
+    public static bool ContainsDuplicatesHashSetApproach<T>(this IEnumerable<T> enumerable)
+    {
+        HashSet<T> knownElements = new();
+        foreach (T element in enumerable)
+        {
+            if (!knownElements.Add(element))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static bool ContainsDuplicatesDistinctCountApproach<T>(this IEnumerable<T> enumerable)
+    {
+        return (enumerable.Count() != enumerable.Distinct().Count());
+    }
+}
